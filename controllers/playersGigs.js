1fc32c618cba361cdb82b093ddfa5c059f8c6b51
kenneth johnston <kennethjohnston@kenneths-Mini.attlocal.net>
2023-06-29 11:16:33 -0400

    engineers by speak

--- controllers/playersGigs.js
+++ controllers/playersGigs.js
@@ -16,4 +16,17 @@ playerGigsController.getPlayers = async (req, res, next) => {
 
 };
 
+playerGigsController.getPlayersBySpeak = async (req, res, next) => {
+    const sid = req.params.sid;
+
+    const queryString = `SELECT e.name AS name, e._id AS id FROM engineers e JOIN engineers_speaks es ON e._id = es.engineer_id JOIN speaks ON speaks.id = es.speak_id AND es.speak_id = ${sid};`;
+    try {
+        const reply = await db.query(queryString);
+        res.locals.engineers = reply.rows; 
+        return next();
+    } catch (err) {
+        console.log('There was a db error')
+    }
+};
+
 module.exports = playerGigsController;
